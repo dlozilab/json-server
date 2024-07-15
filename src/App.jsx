c19c5d634dcdf2a96fff9138be406acfb9f150f2
dlozilab <dlozi@mlab.co.za>
2024-07-15 14:55:20 +0200

    chores

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const traineeDataURL = "http://localhost:4000/trainees/";
+  const [list,setList]=useState([]);
+
+  useEffect(() => {
+    console.log("effect run")
+    const fecthTraineeData = async () => {
+      console.log("running fecthTraineeData");
+
+      try {
+        const response = await fetch(traineeDataURL);
+        const traineeData = await response.json();
+        setList(traineeData);
+        console.log("Below is the trainee data:", traineeData);
+      } catch (err) {
+        alert(err);
+      }
+    };
+
+    (async ()=> await fecthTraineeData())()
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Check logs on the console for data {list.length===0?"name":list[0].name}</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
